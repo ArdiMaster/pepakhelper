52c1bee2a088a15b891f5eceb94c49d54dd81e43
ArdiMaster <ardimaster@outlook.de>
2017-09-25 00:24:59 +0200

    Turns out maxUses 0 actually means unbreakable.

--- src/main/java/me/ardimaster/pepak/helper/item/PEHItems.java
+++ src/main/java/me/ardimaster/pepak/helper/item/PEHItems.java
@@ -7,7 +7,7 @@ import net.minecraft.item.ItemSword;
 import net.minecraftforge.common.util.EnumHelper;
 
 public final class PEHItems {
-    public static Item.ToolMaterial materialA = EnumHelper.addToolMaterial("materialA", 4, 0, 25.0F, 340282347.0F, 0);
+    public static Item.ToolMaterial materialA = EnumHelper.addToolMaterial("materialA", 4, 1, 25.0F, 340282347.0F, 0);
 
     public static ItemSword swordA;
 
